06b471bedd1cf6aeb730db17bbc68ae99f198c5a
50512 <me@50512.dev>
2026-09-23 21:27:12 -0500

    fix bug in /ready log exception

--- api/app/main.py
+++ api/app/main.py
@@ -89,7 +89,7 @@ async def enforce_https(request: Request, call_next):
     if request.url.path in EXCLUDED_MIDDLEWARE_PATHS:
         response = await call_next(request)
         if request.url.path == "/ready" and LOG_LEVEL != "DEBUG":
-            return await call_next(request)
+            return response
 
     elif not is_valid_ip(client_ip):
         response = JSONResponse(
